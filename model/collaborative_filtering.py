a2b16abc1a2c60b00755234558d31134045cd854
SEOLMIN KWON <stat_kwon@naver.com>
2021-07-30 22:45:37 +0900

    [#6] Temp data에 대한 협업필터링 Test

--- model/collaborative_filtering.py
+++ model/collaborative_filtering.py
@@ -4,17 +4,15 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 
 def make_input_data(data_path):
-    food_info = pd.read_csv(data_path)
+    food_info = pd.read_csv(data_path,encoding="utf-8")
     food_info['soup'] = food_info.apply(create_soup, axis=1)
+    return food_info
 
 
 def create_soup(food_info):
-    '''
-    type(food_info['ingredients']) =List<string>
-    type(food_info['instagram_info']) =List<string>
-    :return string
-    '''
-    return ' '.join(food_info['ingredients']) + ' ' + ' '.join(food_info['instagram_info'])
+    ing = food_info['ingredients'].replace(",","").replace("[","").replace("]","").replace("'","")
+    insta = food_info['instagram'].replace(",","").replace("[","").replace("]","").replace("'","")
+    return ing+insta
 
 
 def make_count_vectorizer(series):
@@ -29,17 +27,23 @@ def get_recommendations(title, indices, cosine_sim, dataframe):
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     sim_scores = sim_scores[1:11]
     movie_indices = [i[0] for i in sim_scores]
-    return dataframe['title'].iloc[movie_indices]
+    return dataframe['title'].iloc[movie_indices], sim_scores
 
 
 def collaborative_filtering(title, data_path):
 
-    food_info = pd.read_csv(data_path)
-    make_input_data(food_info)
+    food_info = make_input_data(data_path)
     count_matrix = make_count_vectorizer(food_info['soup'])
 
     cosine_sim = cosine_similarity(count_matrix, count_matrix)
     indices = pd.Series(food_info.index, index=food_info['title'])
 
-    results = get_recommendations(title, indices, cosine_sim, food_info)
-    return list(results)
+    results, sim_scores = get_recommendations(title, indices, cosine_sim, food_info)
+    return results.tolist(), sim_scores
+
+if __name__ == "__main__":
+    recommendations, scores = collaborative_filtering("닭볶음탕", "./temp_data.csv")
+    print(recommendations)
+    print(scores)
+
+    # 여러개 음식이 들어왔을때에 대한 로직 추가 필요
