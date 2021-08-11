3bb404a995a6fbd1f34eba063988145f7c84f032
SEOLMIN KWON <stat_kwon@naver.com>
2021-08-11 17:00:58 +0900

    [#12] feat: 최종 DB 구축 및 유사도 기반 퍼센트 로직 추가

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+from model.collaborative_filtering import foods_collaborative_filtering, DATA_ROOT
+
 app = Flask(__name__)
 CORS(app)
 
 
-def get_ml_results(selected_foods):
-    pass
-
-
 @app.route('/api/ml-servers', methods=['POST'])
 def find_ml_results():
-    selected_foods = request.form['foods']
-    recommended_foods = get_ml_results(selected_foods)
+    print(request.get_json())
+    selected_foods = request.get_json()['foods']
+    print(selected_foods)
+    results_name_list, result_score_list = foods_collaborative_filtering(selected_foods, DATA_ROOT)
 
     return jsonify(
         {
-            "foods": recommended_foods
+            "foods": results_name_list,
+            "scores": result_score_list
         }
     )
 
